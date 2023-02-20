9eb4d3c30addf59b8fc5ab8cb7eaaed0aa05ea9a
Dilip Singh <101004823+dilipsingh076@users.noreply.github.com>
2023-02-20 17:50:01 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const { mongoose } = require("mongoose")
-const mangoose = require("mongoose")
-const MONGO_URL = `mongodb+srv://dilipsinghf:7665135229@cluster0.bk0q22z.mongodb.net/?retryWrites=true&w=majority`
-const connection = mongoose.connect(MONGO_URL)
-
-
-module.exports = {connection}
